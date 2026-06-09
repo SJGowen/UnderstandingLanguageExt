ddcecfa23e9b6c885ee22492478b060ea455ece4
Stephen <sjgowen@gmail.com>
2026-06-09 12:28:13 +0100

    Added a static class BoxMethods, containing a Select method. Used the Select within Program.cs.

---
+++ BoxTypeMonad/BoxMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxTypeMonad;
+
+public static class BoxMethods
+{
+    /// <summary>
+    /// Transforms the contents of a Box, in a user defined way
+    /// </summary>
+    /// <typeparam name="TA">The type of the thing in the box to start with</typeparam>
+    /// <typeparam name="TB">The result type that the transforming function transforms to</typeparam>
+    /// <param name="box">The Box that the extension method with work on</param>
+    /// <param name="map">User defined way to transform the contents of the box</param>
+    /// <returns>The results of the transformation, put back into a box</returns>
+    public static Box<TB> Select<TA, TB>(this Box<TA> box, Func<TA, TB> map)
+    {
+        if (box.IsEmpty)
+        {
+            return new Box<TB>();
+        }
+
+        TB transformedItem = map(box.Item);
+        return new Box<TB>(transformedItem);
+    }
+}
--- BoxTypeMonad/Program.cs
+++ BoxTypeMonad/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace BoxTypeMonad;
+﻿using System.Diagnostics;
+
+namespace BoxTypeMonad;
 
 internal class Program
 {
@@ -6,5 +8,9 @@ internal class Program
     {
         var myNumberBox1 = new Box<int>();
         myNumberBox1.Item = 99;
+        Debug.WriteLine($"myNumberBox1.Item: {myNumberBox1.Item}");
+
+        Box<int> result2 = myNumberBox1.Select(x => x + 1);
+        Debug.WriteLine($"result2.Item: {result2.Item}");
     }
 }
