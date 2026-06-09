6b3373e96618e132d5201b09c6b269b4244e2db2
Stephen <sjgowen@gmail.com>
2026-06-09 16:30:12 +0100

    Added the Bind and Map methods. Note there is no difference between Select and Map. Bind does pretty much the same but there is no Boxing of the result. Program.cs now uses all of the methods.

--- BoxTypeMonad/BoxMethods.cs
+++ BoxTypeMonad/BoxMethods.cs
@@ -8,20 +8,62 @@ public static class BoxMethods
 {
     /// <summary>
     /// Transforms the contents of a Box, in a user defined way
+    /// by using the Validate, Extract, Transform and Lift steps.
     /// </summary>
     /// <typeparam name="TA">The type of the thing in the box to start with</typeparam>
     /// <typeparam name="TB">The result type that the transforming function transforms to</typeparam>
     /// <param name="box">The Box that the extension method with work on</param>
     /// <param name="map">User defined way to transform the contents of the box</param>
     /// <returns>The results of the transformation, put back into a box</returns>
-    public static Box<TB> Select<TA, TB>(this Box<TA> box, Func<TA, TB> map)
+    public static Box<TB> Select<TA, TB>(this Box<TA> box, Func<TA, TB> select)
     {
-        if (box.IsEmpty)
-        {
-            return new Box<TB>();
-        }
+        // Validate
+        if (box.IsEmpty) return new Box<TB>();
 
-        TB transformedItem = map(box.Item);
-        return new Box<TB>(transformedItem);
+        // Extract
+        var extracted = box.Item;
+
+        // Transform
+        TB transformed = select(extracted);
+
+        // Lift
+        return new Box<TB>(transformed);
+    }
+
+    /// <summary>
+    /// Validate, Extract, Tranform and Lift
+    /// Check/Validate then transform to T and Lift into Box<t>
+    /// </summary>
+    public static Box<TB> Bind<TA, TB>(this Box<TA> box, Func<TA, Box<TB>> bind)
+    {
+        // Validate
+        if (box.IsEmpty) return new Box<TB>();
+
+        // Extract
+        TA extracted = box.Item;
+
+        // Transform
+        Box<TB> transformedAndLifted = bind(extracted);
+
+        // Lift
+        return transformedAndLifted;
+    }
+
+    /// <summary>
+    /// Validate, Extract, Tranform and automatic Lift
+    /// </summary>
+    public static Box<TB> Map<TA, TB>(this Box<TA> box, Func<TA, TB> map)
+    {
+        // Validate
+        if (box.IsEmpty) return new Box<TB>();
+
+        // Extract
+        TA extracted = box.Item;
+
+        // Transform
+        TB transformed = map(extracted);
+
+        // Lift
+        return new Box<TB>(transformed);
     }
 }
--- BoxTypeMonad/Program.cs
+++ BoxTypeMonad/Program.cs
@@ -10,10 +10,32 @@ public class Program
         myNumberBox1.Item = 99;
         Debug.WriteLine($"myNumberBox1.Item: {myNumberBox1.Item}");
 
-        Box<int> result = myNumberBox1.Select(x => x + 1);
+        Box<int> selectResult = myNumberBox1.Select(x => x + 1);
         // The above uses the Linq Fluent syntax. An alternative is to use the Query syntax:
         // var result = from number in myNumberBox1
         //              select number + 1;
-        Debug.WriteLine($"result.Item: {result.Item}");
+        Debug.WriteLine($"selectedResult.Item: {selectResult.Item}");
+
+        Box<int[]> numbers1 = new([ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]);
+        Box<int[]> numbers2 = new([ 11, 12, 13, 14, 15, 16, 17, 18, 19, 20 ]);
+
+        Box<int[]> bindResult = numbers1.Bind(contents => MyFunction(contents));
+        Box<int[]> mapResult = numbers2.Map(contents => MyFunction2(contents));
+
+        Debug.WriteLine($"bindResult.Item: {string.Join(", ", bindResult.Item)}");
+        Debug.WriteLine($"mapResult.Item: {string.Join(", ", mapResult.Item)}");
+
+        Box<string> mapStringResult = numbers1.Map(contents => "I'm a string!");
+        Debug.WriteLine($"mapStringResult.Item: {mapStringResult.Item}");
+    }
+
+    private static Box<int[]> MyFunction(int[] integerArray)
+    {
+        return new Box<int[]>([1, 2]);
+    }
+
+    private static int[] MyFunction2(int[] integerArray)
+    {
+        return new int[] { 3, 4, 5 };
     }
 }
